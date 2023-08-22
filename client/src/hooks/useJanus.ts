482be26928429f0c3670bdc42e46a6848af2f49a
H Z Wei <hzwei92@gmail.com>
2023-08-22 09:46:47 -0700

    enable switching between videorooms

--- client/src/hooks/useJanus.ts
+++ client/src/hooks/useJanus.ts
@@ -11,7 +11,7 @@ let janus = null as Janus | null;
 let sfutest = null as JanusJS.PluginHandle | null;
 let opaqueId = "videoroomtest-" + Janus.randomString(12);
 
-let myroom = 1234;
+let myroom = null as number | null;
 let myusername = null as any;
 let myid = null as any;
 let mystream = null as any;
@@ -70,25 +70,20 @@ const useJanus = () => {
     }
   }, []);
 
-const joinRoom = (room: number, id: number, username: string) => {
-  myroom = room;
-  myid = id;
-  myusername = username;
-
-  let register = {
-    request: "join",
-    id: myid,
-    room: myroom,
-    ptype: "publisher",
-    display: myusername,
-  };
-
-  let exists = {
-    request: "exists",
-    room: myroom,
-  };
-
   const registerRoom = () => {
+    let register = {
+      request: "join",
+      id: myid,
+      room: myroom,
+      ptype: "publisher",
+      display: myusername,
+    };
+
+    let exists = {
+      request: "exists",
+      room: myroom,
+    };
+  
     sfutest?.send({
       message: exists,
       success: (res) => {
@@ -130,261 +125,265 @@ const joinRoom = (room: number, id: number, username: string) => {
     });
   }
 
-  if (!sfutest) {
-    janus?.attach({
-      plugin: 'janus.plugin.videoroom',
-      opaqueId: 'videoroomtest',
-      success: (pluginHandle) => {
-        setRefresh(prev => !prev);
-        sfutest = pluginHandle;
-        Janus.log("Plugin attached! (" + sfutest.getPlugin() + ", id=" + sfutest.getId() + ")");
-        Janus.log("  -- This is a publisher/manager");
-        registerRoom();
-      },
-      error: (err) => {
-        Janus.error('Janus plugin error', err);
-      },
-      consentDialog: (on) => {
-        Janus.debug('Consent dialog should be ' + (on ? 'on' : 'off') + ' now');
-      },
-      iceState: (state) => {
-        Janus.log('ICE state changed to', state);
-      },
-      mediaState: (medium, on, mid) => {
-        Janus.log("Janus " + (on ? "started" : "stopped") + " receiving our " + medium + " (mid=" + mid + ")");
-      },
-      webrtcState: (on) => {
-        Janus.log("Janus says our WebRTC PeerConnection is " + (on ? "up" : "down") + " now");
-      },
-      slowLink: (uplink, lost, mid) => {
-        Janus.warn("Janus reports problems " + (uplink ? "sending" : "receiving") +
-          " packets on mid " + mid + " (" + lost + " lost packets)");
-      },
-      onmessage: (msg, jsep) => {
-        setRefresh(prev => !prev);
-        Janus.debug(" ::: Got a message (publisher) :::", msg);
-        let event = msg["videoroom"];
-        Janus.debug("Event: " + event);
-        if (event != undefined && event != null) {
-          if (event === "joined") {
-            // Publisher/manager created, negotiate WebRTC and attach to existing feeds, if any
-            myid = msg["id"];
-            mypvtid = msg["private_id"];
-            Janus.log("Successfully joined room " + msg["room"] + " with ID " + myid);
-            dispatch(activateChannel(msg["room"]));
-            if (subscriber_mode) {
-  
-            }
-            else {
-              publishOwnFeed(true);
-            }
-            // Any new feed to attach to?
-            if (msg["publishers"]) {
-              let list = msg["publishers"];
-              Janus.debug("Got a list of available publishers/feeds:", list);
-              let sources = null;
-              for (let f in list) {
-                if (list[f]["dummy"]) 
-                  continue;
-                let id = list[f]["id"];
-                let display = list[f]["display"];
-                let streams = list[f]["streams"];
-                for (let i in streams) {
-                  let stream = streams[i];
-                  stream["id"] = id;
-                  stream["display"] = display;
-                }
-                let slot = feedStreams[id] ? feedStreams[id]?.slot : null;
-                let remoteVideos = feedStreams[id] ? feedStreams[id]?.remoteVideos : 0;
-                feedStreams[id] = {
-                  id: id,
-                  display: display,
-                  streams: streams,
-                  slot: slot,
-                  remoteVideos: remoteVideos
-                }
-                Janus.debug("  >> [" + id + "] " + display + ":", streams);
-                if(!sources) {
-                  sources = [];
-                }
-                sources.push(streams);
+const joinRoom = (room: number, id: number, username: string) => {
+  myroom = room;
+  myid = id;
+  myusername = username;
+
+  if (sfutest) {
+    sfutest.hangup();
+  }
+  janus?.attach({
+    plugin: 'janus.plugin.videoroom',
+    opaqueId: 'videoroomtest',
+    success: (pluginHandle) => {
+      setRefresh(prev => !prev);
+      sfutest = pluginHandle;
+      Janus.log("Plugin attached! (" + sfutest.getPlugin() + ", id=" + sfutest.getId() + ")");
+      Janus.log("  -- This is a publisher/manager");
+      registerRoom();
+    },
+    error: (err) => {
+      Janus.error('Janus plugin error', err);
+    },
+    consentDialog: (on) => {
+      Janus.debug('Consent dialog should be ' + (on ? 'on' : 'off') + ' now');
+    },
+    iceState: (state) => {
+      Janus.log('ICE state changed to', state);
+    },
+    mediaState: (medium, on, mid) => {
+      Janus.log("Janus " + (on ? "started" : "stopped") + " receiving our " + medium + " (mid=" + mid + ")");
+    },
+    webrtcState: (on) => {
+      Janus.log("Janus says our WebRTC PeerConnection is " + (on ? "up" : "down") + " now");
+    },
+    slowLink: (uplink, lost, mid) => {
+      Janus.warn("Janus reports problems " + (uplink ? "sending" : "receiving") +
+        " packets on mid " + mid + " (" + lost + " lost packets)");
+    },
+    onmessage: (msg, jsep) => {
+      setRefresh(prev => !prev);
+      Janus.debug(" ::: Got a message (publisher) :::", msg);
+      let event = msg["videoroom"];
+      Janus.debug("Event: " + event);
+      if (event != undefined && event != null) {
+        if (event === "joined") {
+          // Publisher/manager created, negotiate WebRTC and attach to existing feeds, if any
+          myid = msg["id"];
+          mypvtid = msg["private_id"];
+          Janus.log("Successfully joined room " + msg["room"] + " with ID " + myid);
+          dispatch(activateChannel(msg["room"]));
+          if (subscriber_mode) {
+
+          }
+          else {
+            publishOwnFeed(true);
+          }
+          // Any new feed to attach to?
+          if (msg["publishers"]) {
+            let list = msg["publishers"];
+            Janus.debug("Got a list of available publishers/feeds:", list);
+            let sources = null;
+            for (let f in list) {
+              if (list[f]["dummy"]) 
+                continue;
+              let id = list[f]["id"];
+              let display = list[f]["display"];
+              let streams = list[f]["streams"];
+              for (let i in streams) {
+                let stream = streams[i];
+                stream["id"] = id;
+                stream["display"] = display;
               }
-              console.log('feedStreams', feedStreams);
-              if (sources) {
-                subscribeTo(sources);
+              let slot = feedStreams[id] ? feedStreams[id]?.slot : null;
+              let remoteVideos = feedStreams[id] ? feedStreams[id]?.remoteVideos : 0;
+              feedStreams[id] = {
+                id: id,
+                display: display,
+                streams: streams,
+                slot: slot,
+                remoteVideos: remoteVideos
               }
+              Janus.debug("  >> [" + id + "] " + display + ":", streams);
+              if(!sources) {
+                sources = [];
+              }
+              sources.push(streams);
+            }
+            console.log('feedStreams', feedStreams);
+            if (sources) {
+              subscribeTo(sources);
             }
           }
-          else if (event === "destroyed") {
-            // The room has been destroyed
-            Janus.warn("The room has been destroyed!");
-            dispatch(activateChannel(null));
+        }
+        else if (event === "destroyed") {
+          // The room has been destroyed
+          Janus.warn("The room has been destroyed!");
+          dispatch(activateChannel(null));
+        }
+        else if (event === "event") {
+          // Any info on our streams or a new feed to attach to?
+          if (msg["streams"]) {
+            let streams = msg["streams"];
+            for(let i in streams) {
+              let stream = streams[i];
+              stream["id"] = myid;
+              stream["display"] = myusername;
+            }
+            feedStreams[myid] = {
+              id: myid,
+              display: myusername,
+              streams: streams
+            }
+            console.log('feedStreams', feedStreams);
           }
-          else if (event === "event") {
-            // Any info on our streams or a new feed to attach to?
-            if (msg["streams"]) {
-              let streams = msg["streams"];
+          else if (msg["publishers"]) {
+            let list = msg["publishers"];
+            Janus.debug("Got a list of available publishers/feeds:", list);
+            let sources = null;
+            for(let f in list) {
+              if(list[f]["dummy"])
+                continue;
+              let id = list[f]["id"];
+              let display = list[f]["display"];
+              let streams = list[f]["streams"];
               for(let i in streams) {
                 let stream = streams[i];
-                stream["id"] = myid;
-                stream["display"] = myusername;
+                stream["id"] = id;
+                stream["display"] = display;
               }
-              feedStreams[myid] = {
-                id: myid,
-                display: myusername,
-                streams: streams
+              let slot = feedStreams[id] ? feedStreams[id].slot : null;
+              let remoteVideos = feedStreams[id] ? feedStreams[id].remoteVideos : 0;
+              feedStreams[id] = {
+                id: id,
+                display: display,
+                streams: streams,
+                slot: slot,
+                remoteVideos: remoteVideos
               }
-              console.log('feedStreams', feedStreams);
+              Janus.debug("  >> [" + id + "] " + display + ":", streams);
+              if(!sources) {
+                sources = [];
+              }
+              sources.push(streams);
             }
-            else if (msg["publishers"]) {
-              let list = msg["publishers"];
-              Janus.debug("Got a list of available publishers/feeds:", list);
-              let sources = null;
-              for(let f in list) {
-                if(list[f]["dummy"])
-                  continue;
-                let id = list[f]["id"];
-                let display = list[f]["display"];
-                let streams = list[f]["streams"];
-                for(let i in streams) {
-                  let stream = streams[i];
-                  stream["id"] = id;
-                  stream["display"] = display;
-                }
-                let slot = feedStreams[id] ? feedStreams[id].slot : null;
-                let remoteVideos = feedStreams[id] ? feedStreams[id].remoteVideos : 0;
-                feedStreams[id] = {
-                  id: id,
-                  display: display,
-                  streams: streams,
-                  slot: slot,
-                  remoteVideos: remoteVideos
-                }
-                Janus.debug("  >> [" + id + "] " + display + ":", streams);
-                if(!sources) {
-                  sources = [];
-                }
-                sources.push(streams);
-              }
-              if(sources) {
-                subscribeTo(sources);
-              }
+            if(sources) {
+              subscribeTo(sources);
             }
-            else if (msg["leaving"]) {
-              // One of the publishers has gone away?
-              let leaving = msg["leaving"];
-              Janus.log("Publisher left: " + leaving);
-              unsubscribeFrom(leaving);
+          }
+          else if (msg["leaving"]) {
+            // One of the publishers has gone away?
+            let leaving = msg["leaving"];
+            Janus.log("Publisher left: " + leaving);
+            unsubscribeFrom(leaving);
+          }
+          else if (msg["unpublished"]) {
+            // One of the publishers has unpublished?
+            let unpublished = msg["unpublished"];
+            Janus.log("Publisher left: " + unpublished);
+            if(unpublished === 'ok') {
+              // That's us
+              sfutest?.hangup();
+              sfutest = null;
+              myroom = null;
+              return;
             }
-            else if (msg["unpublished"]) {
-              // One of the publishers has unpublished?
-              let unpublished = msg["unpublished"];
-              Janus.log("Publisher left: " + unpublished);
-              if(unpublished === 'ok') {
-                // That's us
-                sfutest?.hangup();
-                sfutest = null;
-                return;
-              }
-              unsubscribeFrom(unpublished);
+            unsubscribeFrom(unpublished);
+          }
+          else if (msg["error"]) {
+            if (msg["error_code"] === 426) {
+              // This is a "no such room" error: give a more meaningful description
+              console.warn("No such room exists");
             }
-            else if (msg["error"]) {
-              if (msg["error_code"] === 426) {
-                // This is a "no such room" error: give a more meaningful description
-                console.warn("No such room exists");
-              }
-              else {
-                console.error(msg["error"]);
-              }
+            else {
+              console.error(msg["error"]);
             }
           }
         }
-        if (jsep) {
-          Janus.debug("Handling SDP as well...", jsep);
-          sfutest?.handleRemoteJsep({ jsep: jsep });
-          let audio = msg["audio_codec"];
-          if (mystream && mystream.getAudioTracks() && mystream.getAudioTracks().length > 0 && !audio) {
-            // Audio has been rejected
-            console.warn("Our audio stream has been rejected, viewers won't hear us");
-          }
-          let video = msg["video_codec"];
-          if (mystream && mystream.getVideoTracks() && mystream.getVideoTracks().length > 0 && !video) {
-            // Video has been rejected
-            console.warn("Our video stream has been rejected, viewers won't see us");
-          }
+      }
+      if (jsep) {
+        Janus.debug("Handling SDP as well...", jsep);
+        sfutest?.handleRemoteJsep({ jsep: jsep });
+        let audio = msg["audio_codec"];
+        if (mystream && mystream.getAudioTracks() && mystream.getAudioTracks().length > 0 && !audio) {
+          // Audio has been rejected
+          console.warn("Our audio stream has been rejected, viewers won't hear us");
         }
-      },
-      onlocaltrack: (track, on) => {
-        setRefresh(prev => !prev);
-        Janus.debug(" ::: Got a local track event :::");
-        Janus.debug("Local track " + (on ? "added" : "removed") + ":", track);
-        let trackId = track.id;
-        if (!on) {
-          // Track removed, get rid of the stream
-          let stream = localTracks[trackId];
-          if (stream) {
-            try {
-              let tracks = stream.getTracks();
-              for (let i in tracks) {
-                let mst = tracks[i];
-                if (mst) {
-                  mst.stop();
-                }
-              }
-            }
-            catch (err){
-              console.warn(err);
-            }
-          }
-          if (track.kind === 'video') {
-            localVideos--;
-          }
-          delete localTracks[trackId];
-          return;
+        let video = msg["video_codec"];
+        if (mystream && mystream.getVideoTracks() && mystream.getVideoTracks().length > 0 && !video) {
+          // Video has been rejected
+          console.warn("Our video stream has been rejected, viewers won't see us");
         }
-        // If we're here, a new track was added
+      }
+    },
+    onlocaltrack: (track, on) => {
+      setRefresh(prev => !prev);
+      Janus.debug(" ::: Got a local track event :::");
+      Janus.debug("Local track " + (on ? "added" : "removed") + ":", track);
+      let trackId = track.id;
+      if (!on) {
+        // Track removed, get rid of the stream
         let stream = localTracks[trackId];
         if (stream) {
-          // We've been here before
-          return;
-        }
-        if (track.kind === "audio") {
-          // We ignore local audio tracks
-          if (localVideos === 0) {
-            // No video, TODO show a placeholder
+          try {
+            let tracks = stream.getTracks();
+            for (let i in tracks) {
+              let mst = tracks[i];
+              if (mst) {
+                mst.stop();
+              }
+            }
+          }
+          catch (err){
+            console.warn(err);
           }
         }
-        else {
-          // New video track: create a stream out of it
-          localVideos++;
-          let stream = new MediaStream([track]);
-          localTracks[trackId] = stream;
-          Janus.log("Created local video stream:", stream);
-          Janus.log(stream.getTracks());
-          Janus.log(stream.getVideoTracks());
+        if (track.kind === 'video') {
+          localVideos--;
         }
-        if (sfutest?.webrtcStuff.pc.iceConnectionState !== "completed" &&
-          sfutest?.webrtcStuff.pc.iceConnectionState !== "connected") {
-            console.log('Publishing...');
+        delete localTracks[trackId];
+        return;
+      }
+      // If we're here, a new track was added
+      let stream = localTracks[trackId];
+      if (stream) {
+        // We've been here before
+        return;
+      }
+      if (track.kind === "audio") {
+        // We ignore local audio tracks
+        if (localVideos === 0) {
+          // No video, TODO show a placeholder
         }
-      },
-      onremotetrack: (track, mid, on) => {
-        // The publisher stream is sendonly, we dont expect anything here
-      },
-      oncleanup: () => {
-        setRefresh(prev => !prev);
-        Janus.log(" ::: Got a cleanup notification; we are unpublished now :::");
-        mystream = null;
-        delete feedStreams[myid];
-        localTracks = {};
-        localVideos = 0;
-        dispatch(activateChannel(null));
-      },
-    });  
-  }
-  else {
-    registerRoom();
-  }
+      }
+      else {
+        // New video track: create a stream out of it
+        localVideos++;
+        let stream = new MediaStream([track]);
+        localTracks[trackId] = stream;
+        Janus.log("Created local video stream:", stream);
+        Janus.log(stream.getTracks());
+        Janus.log(stream.getVideoTracks());
+      }
+      if (sfutest?.webrtcStuff.pc.iceConnectionState !== "completed" &&
+        sfutest?.webrtcStuff.pc.iceConnectionState !== "connected") {
+          console.log('Publishing...');
+      }
+    },
+    onremotetrack: (track, mid, on) => {
+      // The publisher stream is sendonly, we dont expect anything here
+    },
+    oncleanup: () => {
+      setRefresh(prev => !prev);
+      Janus.log(" ::: Got a cleanup notification; we are unpublished now :::");
+      mystream = null;
+      delete feedStreams[myid];
+      localTracks = {};
+      localVideos = 0;
+      dispatch(activateChannel(null));
+    },
+  });
 }
 
 const publishOwnFeed = (useAudio: boolean) => {
